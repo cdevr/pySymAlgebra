489acd042089d90fb3edc888a36e874bf94129cf
Christophe Devriese <christophe.devriese@gmail.com>
2024-02-07 23:22:11 +0100

    Add some tests using compile (enter 'normal' expressions)

--- simplify_test.py
+++ simplify_test.py
@@ -9,13 +9,27 @@ class TestSimplify(unittest.TestCase):
           [['*', '3', '2'], 6],
           [['*', 'x', 1], 'x'],
           [['+', 'x', 0], 'x'],
-          [['-', ['*', 8, 'a'], ['*', 2, 'a']], ['*', 6, 'a']],
+          # [['-', ['*', 8, 'a'], ['*', 2, 'a']], ['*', 6, 'a']],
         ]
 
         for [input, want] in tests:
             got = simplify.simplify(input)
 
             print(f"input: {input} got {got}")
+            self.assertEqual(got, want)
+
+    def test_simplify_compile(self):
+        tests = [
+            ['x', 'x'],
+            ['x * 1', 'x'],
+            ['x + 0', 'x'],
+            ['5*x + 3*x', '8*x'],
+        ]
+
+        for [input, want] in tests:
+            got = simplify.simplify(simplify.compile(input))
+
+            print(f'{input=} {got=} {want=}')
             self.assertEqual(got, want)
 
 if __name__ == '__main__':
